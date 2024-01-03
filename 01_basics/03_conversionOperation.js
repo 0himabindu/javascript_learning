94383f5c3d3b42d84a05de4977b6b94cec66c247
Hima Bindu <96619687+0himabindu@users.noreply.github.com>
2024-01-03 16:09:12 +0000

    03_learning

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+let score = "33abc";
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn =  "hima";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+let number = 33;
+let stringNum = String(number);
+//console.log(stringNum);
+//console.log(typeof stringNum);
+
+//*******************operations*****************************/
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*3);
+console.log(2/3);
+console.log(2%3); */
+
+let str1 = "hima"
+let str2 = " bindu"
+let str3 = str1 + str2
+console.log(str3);
+
+
+
+
+
+
