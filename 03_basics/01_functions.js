93a9c9106034bf1c5b15ce4e5d8b04802eb65c48
Hima Bindu <96619687+0himabindu@users.noreply.github.com>
2024-01-07 17:46:49 +0000

    ghjk

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,25 @@
+function namerepeat(){
+    console.log("h");
+    console.log("i");
+    console.log("m");
+    console.log("a");
+    console.log("E");
+}
+
+//namerepeat();
+
+// function addTwoNumbers (num1 , num2){
+//     console.log(num1 + num2);
+// }
+// addTwoNumbers(4575,48542)
+
+
+function addTwoNumbers (num1 , num2){
+   let result = num1+num2;
+   console.log("Hima");
+   return result;
+}
+  
+   
+   const result = addTwoNumbers(3,4);
+   console.log("Result :", result);
