da65a5244783b089b50b865bf760cfdc3429bb7b
Hima Bindu <96619687+0himabindu@users.noreply.github.com>
2024-01-24 15:28:28 +0000

    changed

--- 07_projects/color-changer/script.js
+++ 07_projects/color-changer/script.js
@@ -2,23 +2,51 @@ const buttons = document.querySelectorAll('.button');
 const body = document.querySelector("body");
 
 //events in browser
+//using if loop
+// buttons.forEach(function(button){
+//     button.addEventListener('click',function(e){
+
+//     if(e.target.id === "grey"){
+//         body.style.backgroundColor = "grey";
+//     }
+//     if(e.target.id==="white"){
+//         body.style.backgroundColor="white";
+//     }
+//     if(e.target.id==="blue"){
+//         body.style.backgroundColor="blue";
+//     }
+//     if(e.target.id==="yellow"){
+//         body.style.backgroundColor="yellow";
+//     }
+//     if(e.target.id==="purple"){
+//         body.style.backgroundColor="purple";
+//     }
+//     });
+// });
+
+//using switch case
+
 buttons.forEach(function(button){
     button.addEventListener('click',function(e){
 
-    if(e.target.id === "grey"){
+switch (e.target.id) {
+    case "grey":
         body.style.backgroundColor = "grey";
-    }
-    if(e.target.id==="white"){
-        body.style.backgroundColor="white";
-    }
-    if(e.target.id==="blue"){
-        body.style.backgroundColor="blue";
-    }
-    if(e.target.id==="yellow"){
-        body.style.backgroundColor="yellow";
-    }
-    if(e.target.id==="purple"){
-        body.style.backgroundColor="purple";
-    }
+        break;
+    case "white":
+        body.style.backgroundColor = "white";
+        break;
+    case "blue":
+        body.style.backgroundColor = "blue";
+        break;
+    case "yellow":
+        body.style.backgroundColor = "yellow";
+        break;
+    case "purple":
+        body.style.backgroundColor = "purple";
+        break;
+    default:
+        break;
+}
     });
 });
